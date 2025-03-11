735892c13bc794fe0935d9bca6b866e41ed8f86c
Maria Kalinichenko <144844634+kamari00@users.noreply.github.com>
2025-03-11 17:01:53 +0100

    Update Home.tsx

--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -1,7 +1,7 @@
 import Header from '../Header/Header.tsx';
 import Footer from '../Footer/Footer.tsx';
 import styles from './Home.module.css';
-import logIn from '../Login/LoginExport.ts'
+
 
 function Home() {
     return (
@@ -9,16 +9,17 @@ function Home() {
             <Header />
             <br/><br/>
             <div className={styles.mainContent}>
-                <h2 className={styles.header}>Welcome to the Simulation</h2>
+            <h1 className={styles.header}>Genetic Algorithms</h1>
+                <h4 className={styles.subheader}>Welcome to the Simulation</h4>
                 <p className={styles.introText}>
                     Evolution: The driving force for diversity and adaptation on our planet. Natural selection ensures that only the strongest survive and pass on the secret of their success to their children.
-                </p>
-                <p className={styles.introText}>
                     Our simulation uses these same principles in the form of genetic algorithms to find the optimal balance between performance and consumption in vehicles.
-                </p>
-                <p className={styles.introText}>
                     For each configuration of parameters, the program returns the Pareto front, a collection of solutions where no value can be improved without worsening another.
                 </p>
+                <div className={styles.pictureContainer}>
+                    <img src="./example.png"className={styles.image} />
+                </div>
+
                 <div className={styles.buttonContainer}>
                     <button className={styles.button} onClick={clickButton}>Proceed to Simulation</button>
                 </div>
@@ -27,16 +28,16 @@ function Home() {
             <div className={styles.faq}>
                 <h3 className={styles.smallHeader}>FAQ</h3>
                 <details className={styles.details}>
-                    <summary className={styles.summary}>What is the purpose of this simulation?</summary>
-                    <p className={styles.detailsText}>This simulation is designed to optimize specific characteristics of a car using a genetic algorithm. A set of predefined parameters is provided to the algorithm, which then iterates through multiple generations, continuously improving until an optimal solution is found.</p>
+                    <summary className={styles.summary}>What is this simulation about?</summary>
+                    <p className={styles.detailsText}>This simulation lets you optimize certain features of a car. For this to happen, a set of Parameters is given to a genetic algotrithm. The algorithm then goes through multiple generations, each one better than the last, until it reaches the optimum.</p>
                 </details>
                 <details className={styles.details}>
                     <summary className={styles.summary}>How do I use the simulation?</summary>
-                    <p className={styles.detailsText}>To begin, navigate to the Data Visualization page. There, you can define the relevant parameters and start the simulation. If you wish to analyze a specific generation in more detail, you can select it from the drop-down menu.</p>
+                    <p className={styles.detailsText}>First, you have to go to the Data Visualization Page. There simply set your Parameters and start the simulation. If you want to take a closer look at a certain generation, you can do that as well. Just pick it from the drop-down menu.</p>
                 </details>
                 <details className={styles.details}>
-                    <summary className={styles.summary}>What do the graphs represent?</summary>
-                    <p className={styles.detailsText}>The overview graph displays all members of the population, organized by generation and consumption. The generation-specific graph provides a more detailed view, where each member of a generation is represented by three points—one for each of the three gears. These points are positioned based on their gear and elasticity.</p>
+                    <summary className={styles.summary}>When i run the simuation i just get that strange graph. What does it mean?</summary>
+                    <p className={styles.detailsText}>In the overview graph you get all members of the population sortet by generation and consuption. In the generation specific graph, each member of the generation gets three points, one for every one of the three gears. These are positioned according to their gear and their elasticity.</p>
                 </details>
             </div>
             <Footer />
@@ -45,8 +46,6 @@ function Home() {
 }
 
 function clickButton() {
-    // if proceed directly to DataVisualzation you will be signed in as "simulator"
-    logIn('')
     window.location.href = "../../visualization.html";
 }
 
